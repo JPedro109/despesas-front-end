bc7674f8fafa83f871b8c1fc9b5e41f262e8a851
João Pedro <joaopedromoraesnsilva@gmail.com>
2022-10-31 18:50:56 -0300

    refactor: delete default styles and add new styles, changes _app and page main

---
+++ src/styles/pages/home.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const InformationContainer = styled.div`
+    margin: 0 auto;
+    text-align: center;
+    padding: 2rem;
+    background-color: #ebecf0;
+    width: 90%;
+    margin-top: 2rem;
+    border: 1px solid #000;
+
+    > span {
+        display: block;
+        margin-top: 1rem;
+        font-weight: 800;
+        font-size: 1.4rem;
+    }
+`;
