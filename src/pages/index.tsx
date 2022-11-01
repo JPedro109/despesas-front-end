eae6fee0d7389b3de4981fcaad3b3b6e128bb4e1
João Pedro <joaopedromoraesnsilva@gmail.com>
2022-11-01 18:51:06 -0300

    refactor: transform index page in login page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,55 @@
-import type { NextPage } from 'next'
+import React from "react";
 
-const Home: NextPage = () => {
+import { PublicRoute } from "../components/PublicRoute";
+import { PagesContainer } from "../components/PagesContainer";
+import { FormTitle } from "../components/FormTitle";
+import { FormInput } from "../components/FormInput";
+import { FormLink } from "../components/FormLink";
+import { Button } from "../components/Button";
+
+import { Form } from "../styles/form";
+
+import { useAuth } from "../providers/AuthProvider";
+
+const Login = () => {
+
+  const { handleLogin, buttonChildren, formValues, setFormValues } = useAuth();
+
   return (
-    <h1>
-        Create Next App
-    </h1>
-  )
-}
+    <>
+      <PagesContainer>
+        <Form onSubmit={handleLogin}>
+          
+          <FormTitle>
+            Login
+          </FormTitle>
 
-export default Home
+          <FormInput 
+            type="email" 
+            name="email" 
+            placeholder="Email" 
+            formValues={formValues} 
+            setFormValues={setFormValues}
+          />
+
+          <FormInput 
+            type="password" 
+            name="password" 
+            placeholder="Senha" 
+            formValues={formValues} 
+            setFormValues={setFormValues} 
+            />
+
+          <Button type="submit">
+            {buttonChildren}
+          </Button>
+
+          <FormLink link="/register">Ainda não tem um cadastro?</FormLink>
+          <FormLink link="/forget-password">Esqueceu sua senha?</FormLink>
+        </Form>
+      </PagesContainer>
+    </>
+  );
+};
+
+export default PublicRoute(Login);
