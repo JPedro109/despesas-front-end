bcc931e692ad1d0d66a30d7a3cfb939787cd005d
João Pedro <joaopedromoraesnsilva@gmail.com>
2022-11-01 18:10:27 -0300

    refactor: create function add  and clean header  and centralize export in unique variable

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,16 +1,27 @@
 import axios from "axios";
 import nookies from "nookies";
 
-export const clientApi = axios.create({
+const clientApi = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
     "Authorization": `Bearer ${nookies.get().tokenDespesas}`
   }
 });
 
-export const serverApi = (context: any) => axios.create({
+const serverApi = (context: any) => axios.create({
   baseURL: process.env.API_URL,
   headers: {
     "Authorization": `Bearer ${nookies.get(context).tokenDespesas}`
   }
 });
+
+const addTokenInAuthorizationHeader = (token: string) => clientApi.defaults.headers.common["Authorization"] = token;
+
+const cleanAuthorizationHeader = () => clientApi.defaults.headers.common["Authorization"] = undefined;
+
+export const api = {
+  client: clientApi,
+  server: serverApi,
+  addTokenInAuthorizationHeader,
+  cleanAuthorizationHeader
+}
