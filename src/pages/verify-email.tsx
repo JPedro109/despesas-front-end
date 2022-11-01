714dd98c2eb6986f0d998826e2e23d40677aef62
João Pedro <joaopedromoraesnsilva@gmail.com>
2022-11-01 18:57:07 -0300

    feat: verify email page

---
+++ src/pages/verify-email.tsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import { useRouter } from 'next/router';
+
+import { PublicRoute } from "../components/PublicRoute";
+import { VerifyEmailTitle } from "../components/VerifyEmailTitle/index";
+
+import { api } from "../services/api";
+import { notification } from "../utils/notification";
+
+const VerifyEmail = () => {
+  const router = useRouter();
+  const [, query] = router.asPath.split("?");
+  const [calledPush, setCalledPush] = useState(true);
+
+  useEffect(() => {
+    if(calledPush) {
+      const handleVerifyEmail = async () => {
+        await api.client
+          .post(`/verify-email?${query}`)
+          .then(() => notification.success("E-mail verificado com sucesso"))
+          .catch(({ response }) =>
+            response
+              ? notification.error(response.data.message)
+              : notification.error("Erro no Servidor")
+          );
+      };
+      handleVerifyEmail();
+      setCalledPush(false);
+      router.push("/");
+    }
+  }, [query, router, calledPush]);
+
+    return ( 
+        <>
+            <VerifyEmailTitle />
+        </>
+     );
+}
+ 
+export default PublicRoute(VerifyEmail);
