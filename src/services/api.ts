31361451cc635a7cf579b241dd1964f19decabb8
João Pedro <joaopedromoraesnsilva@gmail.com>
2022-11-14 13:02:11 -0300

    refactor: api service

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,23 +1,20 @@
+import { cookies } from "../utils/cookies";
 import axios from "axios";
-import nookies from "nookies";
 
-const clientApi = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
-  headers: {
-    "Authorization": `Bearer ${nookies.get().tokenDespesas}`
-  }
-});
+const clientApi = axios.create();
+clientApi.defaults.baseURL = process.env.NEXT_PUBLIC_API_URL;
+clientApi.defaults.headers.common["Authorization"] = `Bearer ${cookies.getAuthCookies().token}`;
 
 const serverApi = (context: any) => axios.create({
   baseURL: process.env.API_URL,
   headers: {
-    "Authorization": `Bearer ${nookies.get(context).tokenDespesas}`
+    "Authorization": `Bearer ${cookies.getAuthCookies(context).token}`
   }
 });
 
-const addTokenInAuthorizationHeader = (token: string) => clientApi.defaults.headers.common["Authorization"] = token;
+const addTokenInAuthorizationHeader = (token: string) => clientApi.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 
-const cleanAuthorizationHeader = () => clientApi.defaults.headers.common["Authorization"] = undefined;
+const cleanAuthorizationHeader = () => clientApi.defaults.headers.common["Authorization"] = null;
 
 export const api = {
   client: clientApi,
